12f3125bb5eaf8ea6a88a69269caab4d8bc40beb
Scott DeVito <scottadevito@gmail.com>
2021-06-03 22:20:34 -0700

    Add the ability to navigate and add an address simply by visiting a url

--- src/pages/address-details-page.tsx
+++ src/pages/address-details-page.tsx
@@ -1,33 +1,74 @@
 import * as React from "react";
-import Web3 from "web3";
-import { AddressesData } from "../types";
+import { Address } from "../types";
 import { useWalletState } from "../components/context/useWalletState";
+import { Web3Context } from "../components/context/useWeb3";
+import { useParams, Link } from "react-router-dom";
 
 export interface AddressDetailsPageProps {}
 
 const AddressDetailsPage: React.FC<AddressDetailsPageProps> = (props) => {
-  const { state } = useWalletState();
-  // TODO Once we have route params, take the address id from the route and persist to state
-  // using the attemptAddAddress util function
-  React.useEffect(() => {
-    // Get address id from route params
-    // const addressId = props.params.addressId;
-    // const {addressId} = props.params;
-    //
-    // Persist address id and related info to context
-    //   // Validate address and attempt to add it
-    // const { attemptAddAddress } = React.useContext(Web3Context);
-  });
+  const { state, dispatch } = useWalletState();
+
+  // Keep track of errors when attempting to get address info
+  const [addAddressError, setAddAddressError] =
+    React.useState<string | boolean>(false);
+
+  let { addressIdFromUrl } = useParams<{ addressIdFromUrl: string }>();
+  const { attemptAddAddress } = React.useContext(Web3Context);
+
+  const handleAttemptAddAddress = async (addressToBeAdded: Address["id"]) => {
+    const addressInfoFromWeb3 = await attemptAddAddress(
+      addressToBeAdded,
+      setAddAddressError,
+      state.addresses
+    );
+
+    // Persist address info to WalletState context
+    !!addressInfoFromWeb3 &&
+      dispatch({
+        type: "add_address",
+        payload: {
+          address: addressInfoFromWeb3,
+        },
+      });
+  };
 
   const getTransactions = async () => {
     // Get transactions
+    console.log("getTransactions");
   };
+
+  // Once we have route params, take the address id from the route and attempt to persist to state
+  React.useEffect(() => {
+    handleAttemptAddAddress(addressIdFromUrl).then(() => {
+      getTransactions();
+    });
+  }, [addressIdFromUrl]);
 
   return (
     <div>
-      <h1>Address Details Page</h1>
-      <p>{state.addresses[0].name}</p>
-      <p>{`${state.addresses[0].amountEth} Eth`}</p>
+      {!!addAddressError ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h2 style={{ color: "red", fontWeight: 700 }}>
+            Error - Address not found
+          </h2>
+          <Link to="/">Click here to add a new address</Link>
+        </div>
+      ) : (
+        <h1>Address Details Page</h1>
+      )}
+
+      {/* <p>{state.addresses[0].name}</p>
+      <p>{`${state.addresses[0].amountEth} Eth`}</p> */}
+      {/* <p>{state.addresses[0].name}</p>
+      <p>{`${state.addresses[0].amountEth} Eth`}</p> */}
     </div>
   );
 };
